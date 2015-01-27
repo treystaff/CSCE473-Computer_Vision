079808efe4e9fd9ea4e5122fd6111c834af99f15
Trey Stafford <treystaff@gmail.com>
2015-01-26 21:01:07 -0600

    Removed use of checkerboard() function
    Removed call to checkerboard(). We are not supposed to use the
    checkerboard function for this assignment.

--- assignment01/CreateCheckerboard.m
+++ assignment01/CreateCheckerboard.m
@@ -1,17 +1,15 @@
 function CreateCheckerboard(outpath)
-%CreateCheckerboard 
+%CreateCheckerboard
+% Arguments: 
+%    outpath (str): the absolute path to where the image will be saved.
+% Author: Trey Stafford
 
-%Create a 256x256 matrix of zeros (zero value = black)
+%Create a white and black 16x16 matrix .
+white = ones(16);
+black = zeros(16);
 
-%Using the checkerboard function.
-% Note checkerboard makes half the board slightly darker. Use of >0.5 to
-% remove.
-img = (checkerboard(16,8) > 0.5); 
-
-%The 3 lines below is how checkerboard() works minus making half the board darker.
-%white = ones(16);
-%black = zeros(16);
-%img = mat2gray(repmat([black white; white black],8))
+%repmat replicates the pattern 8x8 times to get the 256x256 image
+img = mat2gray(repmat([black white; white black],8))
 
 %Convert the matrix to a matlab image and save.
 imwrite(img,outpath);
